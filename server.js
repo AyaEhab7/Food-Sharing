a0994e0b8a6a8f5eeb2532844921ca5e20a97052
aya ehab <ayaaehab1999@gmail.com>
2024-10-29 13:43:26 +0300

    add css to navbar

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const app = express();
 const port = process.env.PORT ? process.env.PORT : '3000';
 const path = require('path');
 
+
 // MIDDLEWARE
 
 // Middleware to parse URL-encoded data from forms
